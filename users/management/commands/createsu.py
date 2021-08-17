66be7912ebb2de48f84ac6a208f2e5abc2ae41cf
doc2kim <81610276+doc2kim@users.noreply.github.com>
2021-08-17 17:49:22 +0900

    superusers

--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -1,5 +1,4 @@
 from django.core.management.base import BaseCommand
-from django_seed import Seed
 from users.models import User
 
 
@@ -11,6 +10,6 @@ class Command(BaseCommand):
         admin = User.objects.get_or_none(username="ebadmin")
         if not admin:
             User.objects.create_superuser("ebadmin", "cameur22@gmail.com", "1234")
-            self.stdout.write(self.style.SUCCESS(f"Superuser Created"))
+            self.stdout.write(self.style.SUCCESS("Superuser Created"))
         else:
-            self.stdout.write(self.style.SUCCESS(f"Superuser Exists"))
+            self.stdout.write(self.style.SUCCESS("Superuser Exists"))
