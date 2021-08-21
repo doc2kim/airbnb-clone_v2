23b84083d24ec380319082da19cd737960a266a4
doc2kim <81610276+doc2kim@users.noreply.github.com>
2021-08-21 16:56:49 +0900

    STATIC_ROOT update

--- config/settings.py
+++ config/settings.py
@@ -151,9 +151,7 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = []
 
 AUTH_USER_MODEL = "users.User"
 
