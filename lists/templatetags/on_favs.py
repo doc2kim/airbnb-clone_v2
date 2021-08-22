f2f4fb1f4aca3b25e812171bb0bb393d4a96f04f
doc2kim <81610276+doc2kim@users.noreply.github.com>
2021-08-22 22:29:43 +0900

    sadf

--- lists/templatetags/on_favs.py
+++ lists/templatetags/on_favs.py
@@ -10,7 +10,4 @@ def on_favs(context, room):
     the_list = list_models.List.objects.get_or_none(
         user=user, name="My Favorites Houses"
     )
-    if the_list is not None:
-        return room in the_list.rooms.all()
-    else:
-        return False
+    return room in the_list.rooms.all()
