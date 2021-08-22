1631ec52067cb0db42ecf58ab82c43a97f57f547
doc2kim <81610276+doc2kim@users.noreply.github.com>
2021-08-22 22:19:49 +0900

    mail

--- users/models.py
+++ users/models.py
@@ -73,7 +73,7 @@ class User(AbstractUser):
                 "emails/verify_email.html", {"secret": secret}
             )
             send_mail(
-                "Verify Airbnb Account",
+                _("Verify Airbnb Account"),
                 strip_tags(html_message),
                 settings.EMAIL_FROM,
                 [self.email],
