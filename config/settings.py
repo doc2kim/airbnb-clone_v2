31b4ea0aa27ed82f774cccd2f882054958ffca4d
doc2kim <81610276+doc2kim@users.noreply.github.com>
2021-08-23 17:32:40 +0900

    autofield

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
