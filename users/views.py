b05665df19f3c00a3e3220e02aecf9b7534f14f0
doc2kim <81610276+doc2kim@users.noreply.github.com>
2021-08-26 15:17:55 +0900

    asdf

--- users/views.py
+++ users/views.py
@@ -181,7 +181,10 @@ def kakao_callback(request):
             profile_image = properties.get("profile_image")
         else:
             nickname = email
-            profile_image = None
+            if properties.get("profile_image") is not None:
+                profile_image = properties.get("profile_image")
+            else:
+                profile_image = None
         try:
             user = models.User.objects.get(email=email)
             if user.login_method != models.User.LOGIN_KAKAO:
