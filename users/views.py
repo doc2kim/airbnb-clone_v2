c20ddda244e7807f52faddd6c35c767fb9033580
doc2kim <81610276+doc2kim@users.noreply.github.com>
2021-08-26 12:29:53 +0900

    asdf

--- users/views.py
+++ users/views.py
@@ -176,7 +176,7 @@ def kakao_callback(request):
             raise KakaoException("Please also give me your email")
         properties = profile_json.get("properties")
         if properties is None:
-            raise KakaoException(print(token_json))
+            raise KakaoException(print(code))
 
         nickname = properties.get("nickname")
         profile_image = properties.get("profile_image")
