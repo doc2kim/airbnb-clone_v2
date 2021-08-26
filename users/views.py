00e4a3bb3b2e7c145195d866d557dd91d0586746
doc2kim <81610276+doc2kim@users.noreply.github.com>
2021-08-26 10:20:18 +0900

    kakao login

--- users/views.py
+++ users/views.py
@@ -174,11 +174,12 @@ def kakao_callback(request):
         email = profile_json.get("kakao_account").get("email", None)
         if email is None:
             raise KakaoException("Please also give me your email")
-        if profile_json.get("properties").get("nickname") is not None:
-            nickname = profile_json.get("properties").get("nickname")
-        else:
-            nickname = email
-        profile_image = profile_json.get("properties").get("profile_image")
+        properties = profile_json.get("properties")
+        print(properties)
+        nickname = properties.get("nickname")
+        print(nickname)
+        profile_image = properties.get("profile_image")
+        print(profile_image)
         try:
             user = models.User.objects.get(email=email)
             if user.login_method != models.User.LOGIN_KAKAO:
